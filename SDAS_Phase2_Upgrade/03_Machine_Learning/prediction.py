502a44dca2f09bf72dbdcf0b0a795744b8427f36
AdrianDias2023 <adriandias2023@github.com>
2026-08-25 20:39:44 +0530

    chore: clean up redundant temporary phase folders, maintaining unified SDAS_Full_Project directory structure

--- SDAS_Phase2_Upgrade/03_Machine_Learning/prediction.py
+++
@@ -1,3 +0,0 @@
-def predict_water_level(model,input_data):
-    prediction=model.predict(input_data)
-    return prediction
--- SDAS_Phase2_Upgrade/03_Machine_Learning/preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def prepare_dataset(path):
-    df=pd.read_csv(path)
-    df=df.dropna()
-    return df
-
-print('Dataset preprocessing module ready')
